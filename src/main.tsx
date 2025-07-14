56eb93b0a1d7c7c0a865423d92c6b1f64c9e2681
Antonio Juevan <antoniojuevan@gmail.com>
2025-07-14 16:24:35 -0300

    fix: atualizar link da licença no componente SLAModal

--- src/main.tsx
+++ src/main.tsx
@@ -190,7 +190,7 @@ function SLAModal({ tasks, onClose, onRefresh }: { tasks: TaskItem[]; onClose: (
           <p className="text-sm text-gray-600">
             © 
             <a 
-              href="https://github.com/USERNAME/REPO/blob/main/LICENSE" 
+              href="https://juevan.github.io/ZeevSLABlocker/main/LICENSE" 
               target="_blank" 
               className="text-blue-600 hover:text-blue-800 underline"
             >
